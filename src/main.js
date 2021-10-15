b47ac848a29027bcd45e8d536cde69ea4ef8ce44
Andreas Lydersen <andreas.lydersen@prediktor.com>
2021-10-15 10:30:31 +0200

    Adding RestoreVariableMappings

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 ping = require("nodes/ping");
 restoreNamespace = require("nodes/restore-namespace");
+restoreVariableMappings = require("./nodes/restore-variable-mappings");
 prediktorEdgeManagementNode = require("./nodes/prediktor-edge-management");
 
 module.exports = function(RED){
     RED.nodes.registerType('ping-namespace', ping.PingNamespaceNode);
     RED.nodes.registerType('restore-namespace', restoreNamespace.RestoreNamespace);
+    RED.nodes.registerType('restore-variable-mappings', restoreVariableMappings.restoreVariableMappings);
     RED.nodes.registerType('prediktor-edge-management', prediktorEdgeManagementNode.PrediktorEdgeManagementNode);
 }
