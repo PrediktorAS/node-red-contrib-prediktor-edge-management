f31fdab6f375186b917e69570cffc6321906530c
Andreas Lydersen <andreas.lydersen@prediktor.com>
2021-11-15 20:02:50 +0100

    Renamed to ping from ping-namespace

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ restoreVariableMappings = require("./nodes/restore-variable-mappings");
 prediktorEdgeManagementNode = require("./nodes/prediktor-edge-management");
 
 module.exports = function(RED){
-    RED.nodes.registerType('ping-namespace', ping.PingNamespaceNode);
+    RED.nodes.registerType('ping', ping.PingService);
     RED.nodes.registerType('restore-namespace', restoreNamespace.RestoreNamespace);
     RED.nodes.registerType('restore-variable-mappings', restoreVariableMappings.restoreVariableMappings);
     RED.nodes.registerType('prediktor-edge-management', prediktorEdgeManagementNode.PrediktorEdgeManagementNode);
